c2d54d4ba21038afa05323dc383f2786257a88af
Kevin Kredit <kevin.kredit@dornerworks.com>
2018-12-19 14:40:16 -0500

    [Webserver] Add HTTP server CGI app source code and makefiles
    This will use the FreeRTOS SDK Platform.h as much of the Xilinx specific code has been removed.

---
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_FAT_TCP_CGI_RISC-V/BuildEnvironment.mk
@@ -0,0 +1,25 @@
+
+BUILD_DIR = ./build
+CROSS_COMPILE_PREFIX = riscv64-unknown-elf
+
+LINKER_SCRIPT = lscript.ld
+#-----------------------------------------------------------
+GCC     = $(CROSS_COMPILE_PREFIX)-gcc
+OBJCOPY = $(CROSS_COMPILE_PREFIX)-objcopy
+OBJDUMP = $(CROSS_COMPILE_PREFIX)-objdump
+AR      = $(CROSS_COMPILE_PREFIX)-ar
+RANLIB  = $(CROSS_COMPILE_PREFIX)-ranlib
+
+OUT_ELF = FreeRTOS-web-server.elf
+CRT0    := UNKNOWN
+
+CFLAGS  = -march=rv32imac -mabi=ilp32 -Wall -O0 -g3 -fmessage-length=0 -MT"$@" -fomit-frame-pointer -fno-strict-aliasing -fno-builtin -D__gracefulExit -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"
+LDFLAGS :=  -march=rv32imac -mabi=ilp32 -nostartfiles -static -Wl,-build-id=none
+LDOPTS  := -Wl,-T -Wl,$(LINKER_SCRIPT)
+LIB_FLAGS := -Wl,--start-group,-lgcc,-lc,--end-group
+
+
+
+# Local Variables:
+# mode: makefile
+# End:
---
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_FAT_TCP_CGI_RISC-V/src/cgi.c
@@ -0,0 +1,66 @@
+/*
+ * File: cgi.c
+ *
+ * Description:
+ *   This file implements CGI setup functions.
+ *
+ * Copyright DornerWorks 2018
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1.  Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2.  Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DORNERWORKS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "FreeRTOS_HTTP_cgi.h"
+#include "task.h"
+
+/* App includes. */
+#include "UDPLoggingPrintf.h"
+#include "cgi.h"
+
+/* Function declarations */
+extern BaseType_t CgiOverflow( char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCgiArgs );
+extern BaseType_t CgiPermissions( char *pcWriteBuffer, size_t xWriteBufferLen,
+                                  const char *pcCgiArgs );
+extern BaseType_t CgiLeakage( char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCgiArgs );
+
+static void CgiRegisterSingle( const char *pcUrl, pdCGI_CALLBACK pxFunc );
+
+/* Function definitions */
+void CgiSetup( void )
+{
+    CgiRegisterSingle( "/cgi-bin/overflow.cgi", CgiOverflow );
+    CgiRegisterSingle( "/cgi-bin/permissions.cgi", CgiPermissions );
+    CgiRegisterSingle( "/cgi-bin/leakage.cgi", CgiLeakage );
+}
+
+static void CgiRegisterSingle( const char *pcUrl, pdCGI_CALLBACK pxFunc )
+{
+    BaseType_t rv;
+
+    CGI_Registry_Entry_t entry = {
+        pcUrl,
+        pxFunc,
+    };
+    rv = FreeRTOS_CGIRegister( &entry );
+    if( rv != 0 )
+    {
+        FreeRTOS_debug_printf( ("Error registering CGI for URL %s\n", pcUrl) );
+    }
+}
