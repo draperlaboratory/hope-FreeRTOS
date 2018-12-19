ee7fff6f1e00e0d13cead75d9aa795ad2a832a20
Kevin Kredit <kevin.kredit@dornerworks.com>
2018-12-19 14:47:47 -0500

    Add HTTP server CGI app source code and makefiles

---
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_FAT_TCP_CGI_RISC-V/BuildEnvironment.mk
@@ -0,0 +1,26 @@
+
+BUILD_DIR = ./build
+CROSS_COMPILE_PREFIX = riscv32-unknown-elf
+
+LINKER_SCRIPT = lscript.ld
+XILINX_SPEC = Xilinx.spec
+#-----------------------------------------------------------
+GCC     = $(CROSS_COMPILE_PREFIX)-gcc
+OBJCOPY = $(CROSS_COMPILE_PREFIX)-objcopy
+OBJDUMP = $(CROSS_COMPILE_PREFIX)-objdump
+AR      = $(CROSS_COMPILE_PREFIX)-ar
+RANLIB  = $(CROSS_COMPILE_PREFIX)-ranlib
+
+OUT_ELF = FreeRTOS-web-server.elf
+CRT0    := UNKNOWN
+
+CFLAGS  = -Wall -O0 -g3 -fmessage-length=0 -MT"$@" -fomit-frame-pointer -fno-strict-aliasing -fno-builtin -D__gracefulExit -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"
+LDFLAGS :=  -nostartfiles -static -Wl,-build-id=none
+LDOPTS  := -Wl,-T -Wl,$(LINKER_SCRIPT)
+LIB_FLAGS := -Wl,--start-group,-lxil,-lgcc,-lc,--end-group
+
+
+
+# Local Variables:
+# mode: makefile
+# End:
