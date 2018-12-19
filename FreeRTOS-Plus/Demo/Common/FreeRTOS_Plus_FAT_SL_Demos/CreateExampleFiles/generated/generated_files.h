6a9f6bcbbd1d0bfb34f93fce6b2d758563b21604
Kevin Kredit <kevin.kredit@dornerworks.com>
2018-12-19 14:26:51 -0500

    Add FAT intiailization code, default files

---
+++ FreeRTOS-Plus/Demo/Common/FreeRTOS_Plus_FAT_SL_Demos/CreateExampleFiles/generated/generated_files.h
@@ -0,0 +1,19 @@
+/* WARNING: Automatically Generated File */
+
+#ifndef GENERATED_FILES
+#define GENERATED_FILES
+
+#include "index_html.h"
+#include "network_security_logo_small_png.h"
+#include "dornerworks_logo_png.h"
+#include "draper_logo_png.h"
+#include "inria_logo_png.h"
+#include "upenn_logo_png.h"
+#include "network_security_logo_smaller_png.h"
+#include "network_security_logo_large_png.h"
+#include "network_security_logo_png.h"
+#include "dover_logo_png.h"
+#include "mit_csail_logo_png.h"
+#include "psu_logo_png.h"
+
+#endif /* GENERATED_FILES */
