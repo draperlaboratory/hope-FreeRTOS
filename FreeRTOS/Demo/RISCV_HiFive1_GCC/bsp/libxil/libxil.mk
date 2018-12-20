c3d7224a4729b47c297b5661df958c4b460b18ea
Nathan Studer <nathan.studer@dornerworks.com>
2018-12-20 11:53:17 -0500

    [HiFive BSP] Add libxil makefile

---
+++ FreeRTOS/Demo/RISCV_HiFive1_GCC/bsp/libxil/libxil.mk
@@ -0,0 +1,42 @@
+# See LICENSE for license details.
+
+ifndef _MK_LIBXIL
+_MK_LIBXIL := # defined
+
+LIBXIL_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+LIBXIL_DIR := $(LIBXIL_DIR:/=)
+
+LIBXIL_SRCS := \
+	libsrc/emacps_v3_2/src/xemacps.c \
+	libsrc/emacps_v3_2/src/xemacps_control.c \
+	libsrc/emacps_v3_2/src/xemacps_intr.c \
+	libsrc/standalone_v5_5/src/xil_assert.c \
+	libsrc/standalone_v5_5/src/xil_stubs.c \
+	libsrc/scugic_v3_3/src/xscugic.c \
+	libsrc/scugic_v3_3/src/xscugic_hw.c \
+	libsrc/scugic_v3_3/src/xscugic_g.c \
+	libsrc/standalone_v5_5/src/xtime_l.c \
+	libsrc/standalone_v5_5/src/xil_io.c \
+	libsrc/standalone_v5_5/src/sleep.c
+
+INCLUDES = -I$(LIBXIL_DIR)/include
+FREERTOS_INCLUDES += $(LIBXIL_DIR)/include
+
+LIBXIL_SRCS := $(foreach f,$(LIBXIL_SRCS),$(LIBXIL_DIR)/$(f))
+LIBXIL_OBJS := $(LIBXIL_SRCS:.c=.o)
+
+LIBXIL := libxil.a
+
+LINK_DEPS += $(LIBXIL_DIR)/$(LIBXIL)
+
+LDFLAGS += -L. -Wl,--start-group -lxil -lc -Wl,--end-group
+
+CLEAN_OBJS += $(LIBXIL_OBJS)
+
+$(LIBXIL_OBJS): %.o: %.c $(HEADERS)
+	$(GCC) $(CFLAGS) $(INCLUDES) -c -o $@ $<
+
+$(LIBXIL_DIR)/$(LIBXIL): $(LIBXIL_OBJS)
+	$(AR) rcs $@ $^
+
+endif # _MK_LIBXIL
