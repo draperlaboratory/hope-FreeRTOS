1cd8f482e945d3c006bd93e5cbc4871d9ba27edd
Michal Podhradsky <mpodhradsky@galois.com>
2019-03-14 14:02:35 -0700

    Clean comments

--- FreeRTOS/Demo/RISC-V_Galois_P1/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_Galois_P1/FreeRTOSConfig.h
@@ -103,17 +103,19 @@
 #define configUSE_MALLOC_FAILED_HOOK	1
 #define configUSE_APPLICATION_TASK_TAG	0
 #define configUSE_COUNTING_SEMAPHORES	1
-#define configGENERATE_RUN_TIME_STATS	1
+// TODO: the stats are not functional yet
+#define configGENERATE_RUN_TIME_STATS	0
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 1
 
 /* Runtime stats definitions */
+// TODO: the stats are not functional yet
 // TODO: these are very rudimentary and might overflow, use only as an example
-#define configUSE_STATS_FORMATTING_FUNCTIONS 1
-extern uint64_t base_timer_value;
-extern uint64_t port_get_current_mtime(void);
-extern void port_init_stats(void);
-#define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() port_init_stats()
-#define portGET_RUN_TIME_COUNTER_VALUE() (uint32_t)(port_get_current_mtime()-base_timer_value)
+// #define configUSE_STATS_FORMATTING_FUNCTIONS 1
+// extern uint64_t base_timer_value;
+// extern uint64_t port_get_current_mtime(void);
+// extern void port_init_stats(void);
+// #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() port_init_stats()
+// #define portGET_RUN_TIME_COUNTER_VALUE() (uint32_t)(port_get_current_mtime()-base_timer_value)
 
 
 
