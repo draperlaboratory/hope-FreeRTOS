96c7d2fc323adde48ac6e540fe739db98c0653a2
John Merrill <jmerrill@draper.com>
2019-04-19 19:19:02 -0400

    refactor JS helper functions into HTML

--- FreeRTOS-Plus/Demo/Common/FreeRTOS_Plus_FAT_SL_Demos/CreateExampleFiles/generated/generated_files.h
+++ FreeRTOS-Plus/Demo/Common/FreeRTOS_Plus_FAT_SL_Demos/CreateExampleFiles/generated/generated_files.h
@@ -4,14 +4,14 @@
 #define GENERATED_FILES
 
 #include "search_results_html.h"
+#include "logout_html.h"
 #include "login_html.h"
-#include "index_html.h"
 #include "add_record_html.h"
 #include "update_user_html.h"
 #include "register_html.h"
 #include "dashboard_html.h"
 #include "user_details_html.h"
+#include "index_html.h"
 #include "search_html.h"
-#include "helpers_js.h"
 
 #endif /* GENERATED_FILES */
