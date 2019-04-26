c1aee16879babd10111e494982c1061693bdf317
Michal Podhradsky <mpodhradsky@galois.com>
2019-04-26 10:35:16 -0700

    Enable TCP and UDP server tasks

--- FreeRTOS/Demo/RISC-V_Galois_P1/demo/main_tcp.c
+++ FreeRTOS/Demo/RISC-V_Galois_P1/demo/main_tcp.c
@@ -123,8 +123,8 @@ connections on the standard echo port (port 7), then echos back any data
 received on that connection.
 */
 #define mainCREATE_SIMPLE_UDP_CLIENT_SERVER_TASKS 1
-#define mainCREATE_TCP_ECHO_TASKS_SINGLE 1
-#define mainCREATE_TCP_ECHO_SERVER_TASK 0
+#define mainCREATE_TCP_ECHO_TASKS_SINGLE 0
+#define mainCREATE_TCP_ECHO_SERVER_TASK 1
 
 /*
  * Just seeds the simple pseudo random number generator.
