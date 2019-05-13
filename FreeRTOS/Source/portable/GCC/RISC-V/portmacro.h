5d8eec08b1734cd22712b8ad4b1e8c30c5575df1
rtel <rtel@1d2547de-c912-0410-9cb9-b8ca96c0e9e2>
2019-05-13 03:14:05 +0000

    Added additional xMessageBufferSpacesAvailable() (plural) to existing xMessageBufferSpaceAvailable() (singular) macro as the documentation muddled both. Added #define portPOINTER_SIZE_TYPE 	uint64_t to the 64-bit RISC-V port layer.
    git-svn-id: https://svn.code.sf.net/p/freertos/code/trunk@2663 1d2547de-c912-0410-9cb9-b8ca96c0e9e2

--- FreeRTOS/Source/portable/GCC/RISC-V/portmacro.h
+++ FreeRTOS/Source/portable/GCC/RISC-V/portmacro.h
@@ -96,10 +96,11 @@ mc_t global_exception_mc;
 
 /* Type definitions. */
 #if __riscv_xlen == 64
-	#define portSTACK_TYPE	uint64_t
-	#define portBASE_TYPE	int64_t
-	#define portUBASE_TYPE	uint64_t
-	#define portMAX_DELAY ( TickType_t ) 0xffffffffffffffffUL
+	#define portSTACK_TYPE			uint64_t
+	#define portBASE_TYPE			int64_t
+	#define portUBASE_TYPE			uint64_t
+	#define portMAX_DELAY 			( TickType_t ) 0xffffffffffffffffUL
+	#define portPOINTER_SIZE_TYPE 	uint64_t
 #elif __riscv_xlen == 32
 	#define portSTACK_TYPE	uint32_t
 	#define portBASE_TYPE	int32_t
