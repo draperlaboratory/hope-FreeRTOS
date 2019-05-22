ee8002514d0fe44fe9ea9cc1db304bd88c129147
Michal Podhradsky <mpodhradsky@galois.com>
2019-05-22 11:45:30 -0700

    [SD] Fixed buffered read, can read filesystem now

--- FreeRTOS/Demo/RISC-V_Galois_P1/demo/main_sd.c
+++ FreeRTOS/Demo/RISC-V_Galois_P1/demo/main_sd.c
@@ -36,10 +36,26 @@
 #include "bsp.h"
 
 #include "ff.h" /* Declarations of FatFs API */
+#include "diskio.h"
 
 void main_sd(void);
+void print_array(char *name, uint8_t *buf, size_t len);
 
 static void prvSdTestTask0(void *pvParameters);
+
+void print_array(char *name, uint8_t *buf, size_t len)
+{
+    printf("%s = [\r\n", name);
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("0x%X, ", buf[i]);
+        if (i % 10 == 0)
+        {
+            printf("\r\n");
+        }
+    }
+    printf("];\r\n");
+}
 
 void main_sd(void)
 {
@@ -57,9 +73,9 @@ static void prvSdTestTask0(void *pvParameters)
     FIL Fil;     /* File object needed for each open file */
     FILINFO FilInfo;
     UINT bw;
-    DIR d;
+    DIR dp;
 
-    
+    //printf(" disk_init = %u\r\n", disk_initialize(0));
 
     // if (f_open(&Fil, "newfile.txt", FA_WRITE | FA_CREATE_ALWAYS) == FR_OK) {	/* Create a file */
     // 	f_write(&Fil, "It works!\r\n", 11, &bw);	/* Write data to the file */
@@ -69,16 +85,64 @@ static void prvSdTestTask0(void *pvParameters)
     //  	} else {
     //  		printf("data write error!\r\n");
     //  	}
-      //} else {
-     // 	printf("f_mount error!\r\n");
-      //}
-    //printf(" disk_init = %u\r\n", disk_initialize(0));
-    //uint64_t data = 42;
-    //uint8_t res = disk_ioctl(0, GET_SECTOR_COUNT, &data);
-    //printf("disk_ioctl: res=%u, data=%llu\r\n", res, data);
+    //} else {
+    // 	printf("f_mount error!\r\n");
+    //}
+    printf(" disk_init = %u\r\n", disk_initialize(0));
+
     uint8_t res;
+    uint64_t data = 42;
+    uint8_t buff[1024] = {0xFF};
+
+    //res = disk_ioctl(0, GET_SECTOR_COUNT, &data);
+    //printf("disk_ioctl GET_SECTOR_COUNT: res=%u, data=%llu\r\n", res, data);
+
+    //res = disk_ioctl(0, CTRL_SYNC, &data);
+    //printf("disk_ioctl CTRL_SYNC: res=%u, data=%llu\r\n", res, data);
+
+    //res = disk_ioctl(0, GET_BLOCK_SIZE, &data);
+    //printf("disk_ioctl GET_BLOCK_SIZE: res=%u, data=%llu\r\n", res, data);
+
+    // memset(buff, 0xFF, sizeof(buff));
+    // res = disk_read(0, buff, 0, 1);
+    // if (!res) {
+    //     printf("read #1 OK: %u\r\n", res);
+    //     print_array("buff", buff, 512);
+    // }
+    // //vTaskDelay(pdMS_TO_TICKS(1000));
+
+    // memset(buff, 0x0, sizeof(buff));
+    // res = disk_read(0, buff, 0, 1);
+    // if (!res) {
+    //     printf("read #2 OK: %u\r\n", res);
+    //     print_array("buff", buff, 512);
+    // }
+    // //vTaskDelay(pdMS_TO_TICKS(1000));
+
+    // memset(buff, 0xA5, sizeof(buff));
+    // res = disk_read(0, buff, 0, 1);
+    // if (!res) {
+    //     printf("read #3 OK: %u\r\n", res);
+    //     print_array("buff", buff, 512);
+    // }
+    // //vTaskDelay(pdMS_TO_TICKS(1000));
+
+    //res = f_mkfs ("0", FM_FAT, 65536, WorkBuf, 65536);
+
     res = f_mount(&FatFs, "", 1);
-    printf("f_mount result = %i\r\n", res);		/* Give a work area to the default drive */  
-    printf("terminating\r\n");
-    //configASSERT(0);
+    printf("f_mount result = %i\r\n", res); /* Give a work area to the default drive */
+
+    res = f_opendir(&dp, "/");
+    printf("f_opendir res = %u\r\n", res);
+
+    res = f_readdir(&dp, &FilInfo);
+    printf("f_readdir res = %u\r\n", res);
+    printf("filename: %s\r\n", FilInfo.fname);
+
+    printf("terminating, res = %u\r\n", res);
+    configASSERT(0);
+    for (;;)
+    {
+        // loop forever
+    }
 }
--- FreeRTOS/Demo/RISC-V_Galois_P1/devices/ff.h
+++ FreeRTOS/Demo/RISC-V_Galois_P1/devices/ff.h
@@ -42,12 +42,12 @@ typedef unsigned __int64 QWORD;
 #elif (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__cplusplus)	/* C99 or later */
 #define FF_INTDEF 2
 #include <stdint.h>
-typedef unsigned int	UINT;	/* int must be 16-bit or 32-bit */
-typedef unsigned char	BYTE;	/* char must be 8-bit */
-typedef uint16_t		WORD;	/* 16-bit unsigned integer */
-typedef uint16_t		WCHAR;	/* 16-bit unsigned integer */
-typedef uint32_t		DWORD;	/* 32-bit unsigned integer */
-typedef uint64_t		QWORD;	/* 64-bit unsigned integer */
+typedef uint32_t        UINT;	/* int must be 16-bit or 32-bit */
+typedef uint8_t         BYTE;	/* char must be 8-bit */
+typedef uint32_t        WORD;	/* 16-bit unsigned integer */
+typedef uint32_t        WCHAR;	/* 16-bit unsigned integer */
+typedef uint64_t        DWORD;	/* 32-bit unsigned integer */
+typedef uint64_t        QWORD;	/* 64-bit unsigned integer */
 #else  	/* Earlier than C99 */
 #define FF_INTDEF 1
 typedef unsigned int	UINT;	/* int must be 16-bit or 32-bit */
