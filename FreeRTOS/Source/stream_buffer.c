1fef97aea4d61e8c6ebb6114a294a2f44cd8e5ec
Michal Podhradsky <mpodhradsky@galois.com>
2019-07-05 15:30:44 -0700

    Silence warnings for stream buffer cast align since it is not relevant

--- FreeRTOS/Source/stream_buffer.c
+++ FreeRTOS/Source/stream_buffer.c
@@ -259,6 +259,7 @@ static void prvInitialiseNewStreamBuffer( StreamBuffer_t * const pxStreamBuffer,
 
 		if( pucAllocatedMemory != NULL )
 		{
+			#pragma GCC diagnostic ignored "-Wcast-align"
 			prvInitialiseNewStreamBuffer( ( StreamBuffer_t * ) pucAllocatedMemory, /* Structure at the start of the allocated memory. */ /*lint !e9087 Safe cast as allocated memory is aligned. */ /*lint !e826 Area is not too small and alignment is guaranteed provided malloc() behaves as expected and returns aligned buffer. */
 										   pucAllocatedMemory + sizeof( StreamBuffer_t ),  /* Storage area follows. */ /*lint !e9016 Indexing past structure valid for uint8_t pointer, also storage area has no alignment requirement. */
 										   xBufferSizeBytes,
@@ -271,7 +272,7 @@ static void prvInitialiseNewStreamBuffer( StreamBuffer_t * const pxStreamBuffer,
 		{
 			traceSTREAM_BUFFER_CREATE_FAILED( xIsMessageBuffer );
 		}
-
+		#pragma GCC diagnostic ignored "-Wcast-align"
 		return ( StreamBufferHandle_t ) pucAllocatedMemory; /*lint !e9087 !e826 Safe cast as allocated memory is aligned. */
 	}
 
