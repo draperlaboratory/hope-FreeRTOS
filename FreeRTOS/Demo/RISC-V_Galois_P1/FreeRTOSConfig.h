33a3a7412fea930b461c38466d671a33061f6a26
Michal Podhradsky <mpodhradsky@galois.com>
2019-07-29 16:36:32 -0700

    Increase stack and heap size

--- FreeRTOS/Demo/RISC-V_Galois_P1/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_Galois_P1/FreeRTOSConfig.h
@@ -90,8 +90,8 @@
 #define configPERIPH_CLOCK_HZ ((uint32_t)(50000000))
 #define configTICK_RATE_HZ ((TickType_t)1000)
 #define configMAX_PRIORITIES (5)
-#define configMINIMAL_STACK_SIZE ((uint32_t)2048) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
-#define configTOTAL_HEAP_SIZE ((size_t)(1000 * 1024))
+#define configMINIMAL_STACK_SIZE ((uint32_t)1024) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
+#define configTOTAL_HEAP_SIZE ((size_t)(512 * 1024))
 #define configMAX_TASK_NAME_LEN (16)
 #define configUSE_TRACE_FACILITY 1
 #define configUSE_16_BIT_TICKS 0
