b2504eb7b2733a6124b5fed6132603053a09ac2a
John Merrill <jmerrill@draper.com>
2019-08-06 17:05:47 -0400

    Enable UART read in hifive BSP

--- FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/env/freedom-e300-hifive1/init.c
+++ FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/env/freedom-e300-hifive1/init.c
@@ -177,10 +177,12 @@ unsigned long get_cpu_freq()
 
 static void uart_init(size_t baud_rate)
 {
+  GPIO_REG(GPIO_OUTPUT_XOR) &= ~IOF0_UART0_MASK;
   GPIO_REG(GPIO_IOF_SEL) &= ~IOF0_UART0_MASK;
   GPIO_REG(GPIO_IOF_EN) |= IOF0_UART0_MASK;
   UART0_REG(UART_REG_DIV) = get_cpu_freq() / baud_rate - 1;
   UART0_REG(UART_REG_TXCTRL) |= UART_TXEN;
+  UART0_REG(UART_REG_RXCTRL) |= UART_RXEN;
 }
 
 
