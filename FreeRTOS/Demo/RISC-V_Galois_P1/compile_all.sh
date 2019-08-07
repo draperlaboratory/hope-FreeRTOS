ce2ff9f02ed7ed937dfd69f91724bb5300dd67bf
Michal Podhradsky <mpodhradsky@galois.com>
2019-08-06 18:04:42 -0700

    Enable CI

---
+++ FreeRTOS/Demo/RISC-V_Galois_P1/compile_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for use_clang in no yes
+do
+    export USE_CLANG=$use_clang
+    for prog in main_blinky main_full main_iic main_sd main_uart main_udp main_tcp
+    do
+        make clean
+        export PROG=$prog
+        make
+        if [ $? -eq 0 ]
+        then
+            echo $USE_CLANG $PROG OK
+        else
+            echo $USE_CLANG $PROG failed
+            exit 1
+        fi
+    done
+done
+make clean
