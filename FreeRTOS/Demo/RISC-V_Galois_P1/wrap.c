f7d7782c77667924d73c42c7797c1981cb6b53e9
John Merrill <jmerrill@draper.com>
2019-09-11 12:27:22 -0400

    Roll back word-alignment changes

--- FreeRTOS/Demo/RISC-V_Galois_P1/wrap.c
+++ FreeRTOS/Demo/RISC-V_Galois_P1/wrap.c
@@ -2,48 +2,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void *__wrap_memcpy( void *pvDest, const void *pvSource, size_t ulBytes )
-{
-  uint32_t *pcDest = ( uint32_t * ) pvDest;
-  uint32_t *pcSource = ( uint32_t * ) pvSource;
-  size_t x;
-
-  if ( ulBytes % sizeof(uint32_t) ) {
-    ulBytes += sizeof(uint32_t);
-  }
-
-  ulBytes /= sizeof(uint32_t);
-
-  for( x = 0; x < ulBytes; x++ )
-  {
-    *pcDest = *pcSource;
-    pcDest++;
-    pcSource++;
-  }
-
-  return pvDest;
-}
-
-void *__wrap_memset( void *pvDest, int iValue, size_t ulBytes )
-{
-  uint32_t *pcDest = ( uint32_t * ) pvDest;
-  size_t x;
-
-  if ( ulBytes % sizeof(uint32_t) ) {
-    ulBytes += sizeof(uint32_t);
-  }
-
-  ulBytes /= sizeof(uint32_t);
-
-  for( x = 0; x < ulBytes; x++ )
-  {
-    *pcDest = (uint32_t) iValue;
-    pcDest++;
-  }
-
-  return pvDest;
-}
-
 int __wrap_printf(const char *s, ...)
 {
   char buf[256];
