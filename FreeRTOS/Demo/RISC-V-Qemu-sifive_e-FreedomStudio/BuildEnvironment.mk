d57948a4b345d61ee0f22f1409af455320153736
JohnPMerrill <john.merrill95@gmail.com>
2020-01-08 12:04:00 -0500

    Update installation for running on VCU118 (#24)
    * WIP: only install FreeRTOS libs and headers
    
    * Install hifive linker script
    
    * Remove unnecessary ldflags from build environment
    
    * WIP link demos against independently-compiled libfreertos
    
    * Enable stdlib in linker
    
    * Install hifive and vcu118 libs
    
    * Rename libfreertos for multiple installed libraries
    
    * Resolve linker issue with dover_metadata section
    
    * Add missing i2c source to libfreertos build
    
    * Fix linker error with libfreertos-hifive
    
    * Rename libfreertos to libfreertos-vcu118

--- FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/BuildEnvironment.mk
+++ FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/BuildEnvironment.mk
@@ -3,12 +3,8 @@
 # Copyright (c) 2019, Dornerworks Ltd.
 #
 
-BUILD_DIR = ./build
 CROSS_COMPILE_PREFIX = riscv32-unknown-elf
 
-SDK_DIR ?= ./freedom-e-sdk
-
-LINKER_SCRIPT = $(SDK_DIR)/env/freedom-e300-hifive1/flash.lds
 #-----------------------------------------------------------
 GCC     = $(ISP_PREFIX)/bin/clang
 OBJCOPY = $(CROSS_COMPILE_PREFIX)-objcopy
@@ -28,7 +24,7 @@ ISP_CFLAGS += -ffunction-sections -fdata-sections -fno-builtin-printf
 ISP_CFLAGS += -DDONT_USE_PLIC -DDONT_USE_M_TIME -Dmalloc\(x\)=pvPortMalloc\(x\) -Dfree\(x\)=vPortFree\(x\)
 ISP_CFLAGS += -include sys/cdefs.h
 ISP_CFLAGS += $(ARCH_FLAGS)
-ISP_CFLAGS += -I ${ISP_PREFIX}/riscv32-unknown-elf/include
+ISP_CFLAGS += -I $(ISP_PREFIX)/riscv32-unknown-elf/include
 # These flags are for outputing *.d dependency files for make
 
 ISP_ASMFLAGS =  -O0 -g3
@@ -38,10 +34,8 @@ ISP_ASMFLAGS += -DportasmMSI_HANDLER=$(MSI_HANDLER)
 ISP_ASMFLAGS += -ffunction-sections -fdata-sections
 ISP_ASMFLAGS += -x assembler-with-cpp
 
-LIBWRAP_SYMS := malloc free \
-	open lseek read write fstat stat close link unlink \
-	execve fork getpid kill wait \
-	isatty times sbrk _exit puts
+LIBWRAP_SYMS := open lseek read write fstat stat close link unlink \
+	execve fork getpid kill wait isatty times sbrk _exit puts
 
 # Linker arguments __________________________________________
 ISP_LDFLAGS := -Xlinker --defsym=__stack_size=1K
@@ -49,8 +43,6 @@ ISP_LDFLAGS += -O0 -g3
 ISP_LDFLAGS += -ffunction-sections -fdata-sections --specs=nano.specs
 ISP_LDFLAGS += -nostartfiles
 ISP_LDFLAGS += -T $(LINKER_SCRIPT)
-ISP_LDFLAGS += -Lbuild/
-ISP_LDFLAGS += -Wl,--start-group -lfreertos -lc -Wl,--end-group
 
 ISP_LDFLAGS += $(foreach s,$(LIBWRAP_SYMS),-Wl,--wrap=$(s))
 ISP_LDFLAGS += $(foreach s,$(LIBWRAP_SYMS),-Wl,--wrap=_$(s))
