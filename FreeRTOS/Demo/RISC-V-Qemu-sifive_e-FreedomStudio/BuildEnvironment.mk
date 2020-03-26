86586d8223ab3611adfd6509d0e97a8106be3b70
Arun Thomas <arun@draper.com>
2020-03-26 00:51:39 -0400

    Use lld

--- FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/BuildEnvironment.mk
+++ FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/BuildEnvironment.mk
@@ -8,25 +8,25 @@ CROSS_COMPILE_PREFIX := riscv64-unknown-elf
 #-----------------------------------------------------------
 GCC     = $(ISP_PREFIX)/bin/clang
 
-OBJCOPY = $(CROSS_COMPILE_PREFIX)-objcopy
-OBJDUMP = $(CROSS_COMPILE_PREFIX)-objdump
-AR      = $(CROSS_COMPILE_PREFIX)-ar
-RANLIB  = $(CROSS_COMPILE_PREFIX)-ranlib
+OBJCOPY = llvm-objcopy
+OBJDUMP = llvm-objdump
+AR      = llvm-ar
+RANLIB  = llvm-ranlib
 GDB     = $(CROSS_COMPILE_PREFIX)-gdb
 
 INTERRUPT_HANDLER = handle_trap
 MSI_HANDLER = as_yet_unhandled
 
 # if using the multi-arch (riscv64-unknown-elf-gcc):
-ARCH_FLAGS = -march=rv32ima -mabi=ilp32 -mcmodel=medium
-AR_FLAGS = --target=elf32-littleriscv
+ARCH_FLAGS = -march=rv32ima -mabi=ilp32 -mcmodel=medium --target=riscv32-unknown-elf
+#AR_FLAGS = --target=elf32-littleriscv
 ifeq ($(ARCH), rv64)
-ARCH_FLAGS = -march=rv64imafd -mabi=lp64d -mcmodel=medany
-AR_FLAGS = --target=elf64-littleriscv
+ARCH_FLAGS = -march=rv64imafd -mabi=lp64d -mcmodel=medany --target=riscv64-unknown-elf
+#AR_FLAGS = --target=elf64-littleriscv
 endif
 
 # Basic ISP_CFLAGS:
-ISP_CFLAGS  = -Wall -Wextra -O0 -g3 -std=gnu11
+ISP_CFLAGS  = -Wall -Wextra -O0 -g3 -std=gnu11 -fuse-ld=lld -mno-relax
 ISP_CFLAGS += -ffunction-sections -fdata-sections -fno-builtin-printf
 ISP_CFLAGS += -DDONT_USE_PLIC -DDONT_USE_M_TIME -Dmalloc\(x\)=pvPortMalloc\(x\) -Dfree\(x\)=vPortFree\(x\)
 ISP_CFLAGS += -include sys/cdefs.h
@@ -34,7 +34,7 @@ ISP_CFLAGS += $(ARCH_FLAGS)
 ISP_CFLAGS += -I $(ISP_PREFIX)/$(CROSS_COMPILE_PREFIX)/include
 # These flags are for outputing *.d dependency files for make
 
-ISP_ASMFLAGS =  -O0 -g3
+ISP_ASMFLAGS =  -O0 -g3 -mno-relax
 ISP_ASMFLAGS += $(ARCH_FLAGS)
 ISP_ASMFLAGS += -DportasmHANDLE_INTERRUPT=$(INTERRUPT_HANDLER)
 ISP_ASMFLAGS += -DportasmMSI_HANDLER=$(MSI_HANDLER)
