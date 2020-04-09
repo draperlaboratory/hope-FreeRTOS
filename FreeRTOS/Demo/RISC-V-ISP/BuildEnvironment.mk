77a5a07f40b53a624ab134c6babf0243756bb250
Nathan Studer <nathan.studer@dornerworks.com>
2020-04-09 07:58:10 -0400

    Move rebase fixups

--- FreeRTOS/Demo/RISC-V-ISP/BuildEnvironment.mk
+++ FreeRTOS/Demo/RISC-V-ISP/BuildEnvironment.mk
@@ -3,37 +3,35 @@
 # Copyright (c) 2019, Dornerworks Ltd.
 #
 
-CROSS_COMPILE_PREFIX := riscv$(ARCH_XLEN)-unknown-elf
 #-----------------------------------------------------------
 GCC     = $(ISP_PREFIX)/bin/clang
-ifeq ($(ARCH), rv64)
-GCC     = $(CROSS_COMPILE_PREFIX)-gcc
-endif
-OBJCOPY = $(CROSS_COMPILE_PREFIX)-objcopy
-OBJDUMP = $(CROSS_COMPILE_PREFIX)-objdump
-AR      = $(CROSS_COMPILE_PREFIX)-ar
-RANLIB  = $(CROSS_COMPILE_PREFIX)-ranlib
-GDB     = $(CROSS_COMPILE_PREFIX)-gdb
+
+OBJCOPY = $(ISP_PREFIX)/bin/llvm-objcopy
+OBJDUMP = $(ISP_PREFIX)/bin/llvm-objdump
+AR      = $(ISP_PREFIX)/bin/llvm-ar
+RANLIB  = $(ISP_PREFIX)/bin/llvm-ranlib
+GDB     = $(ISP_PREFIX)/bin/riscv64-unknown-elf-gdb
 
 INTERRUPT_HANDLER = handle_trap
 MSI_HANDLER = as_yet_unhandled
 
 # if using the multi-arch (riscv64-unknown-elf-gcc):
-ARCH_FLAGS = -march=rv32ima -mabi=ilp32 -mcmodel=medium
-ifeq ($(ARCH), rv64)
-ARCH_FLAGS = -march=rv64imafd -mabi=lp64d -mcmodel=medany
+ifneq ($(ARCH), rv64)
+ARCH_FLAGS = -march=rv32ima -mabi=ilp32 -mcmodel=medium --target=riscv32-unknown-elf
+else
+ARCH_FLAGS = -march=rv64imafd -mabi=lp64d -mcmodel=medany --target=riscv64-unknown-elf
 endif
 
 # Basic ISP_CFLAGS:
-ISP_CFLAGS  = -Wall -Wextra -O0 -g3 -std=gnu11
+ISP_CFLAGS  = -Wall -Wextra -O0 -g3 -std=gnu11 -mno-relax
 ISP_CFLAGS += -ffunction-sections -fdata-sections -fno-builtin-printf
 ISP_CFLAGS += -DDONT_USE_PLIC -DDONT_USE_M_TIME -Dmalloc\(x\)=pvPortMalloc\(x\) -Dfree\(x\)=vPortFree\(x\)
 ISP_CFLAGS += -include sys/cdefs.h
 ISP_CFLAGS += $(ARCH_FLAGS)
-ISP_CFLAGS += -I $(ISP_PREFIX)/$(CROSS_COMPILE_PREFIX)/include
+ISP_CFLAGS += -I $(ISP_PREFIX)/clang_sysroot/riscv$(ARCH_XLEN)-unknown-elf/include
 # These flags are for outputing *.d dependency files for make
 
-ISP_ASMFLAGS =  -O0 -g3
+ISP_ASMFLAGS =  -O0 -g3 -mno-relax
 ISP_ASMFLAGS += $(ARCH_FLAGS)
 ISP_ASMFLAGS += -DportasmHANDLE_INTERRUPT=$(INTERRUPT_HANDLER)
 ISP_ASMFLAGS += -DportasmMSI_HANDLER=$(MSI_HANDLER)
@@ -48,7 +46,7 @@ ISP_LDFLAGS := -Xlinker --defsym=__stack_size=1K
 ISP_LDFLAGS += -O0 -g3
 ISP_LDFLAGS += -ffunction-sections -fdata-sections --specs=nano.specs
 ISP_LDFLAGS += -nostartfiles
-ISP_LDFLAGS += -T $(LINKER_SCRIPT)
+ISP_LDFLAGS += -T $(LINKER_SCRIPT) -fuse-ld=lld
 
 ISP_LDFLAGS += $(foreach s,$(LIBWRAP_SYMS),-Wl,--wrap=$(s))
 ISP_LDFLAGS += $(foreach s,$(LIBWRAP_SYMS),-Wl,--wrap=_$(s))
