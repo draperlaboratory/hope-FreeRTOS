56d47f0f8bcdf62da975898e716fce2385423d9b
Michal Podhradsky <mpodhradsky@galois.com>
2021-02-15 18:59:19 -0800

    Remove Galois demo

--- FreeRTOS/Demo/RISC-V_Galois_P1/demo/cpptest.cpp
+++
@@ -1,7 +0,0 @@
-// C++ code:
-#include <cstdio>
-extern "C" int f(int);
-int f(int i) { 
-    printf("Hello world from C++\r\n");
-    return i+1;
-    }
--- FreeRTOS/Demo/RISC-V_Galois_P1/protocols/include/peekpoke.h
+++
@@ -1,4 +0,0 @@
-#include "FreeRTOS_TCP_server.h"
-#include "FreeRTOS_server_private.h"
-
-extern size_t peekPokeHandler( HTTPClient_t *pxClient, BaseType_t xIndex, const char *pcURLData, char *pcOutputBuffer, size_t uxBufferLength );
